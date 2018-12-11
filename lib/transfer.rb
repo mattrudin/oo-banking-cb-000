744ad954ac9f919493ce57dc3eeb32337343dfc6
Matthias Rudin <matthias.rudin@live.com>
2018-12-11 19:04:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :status, :amount
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.sender.status == "open" && self.receiver.status == "open" ? true : false
+  end
 end
